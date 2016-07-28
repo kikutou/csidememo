366996762113ae18750e6341d9e6809cdf6f3256
MAMOL <wangrunbo921@gmail.com>
2016-07-28 22:47:19 +0900

    バグ修正

--- public/javascripts/modify.js
+++ public/javascripts/modify.js
@@ -18,4 +18,32 @@ $(function () {
         });
         $("textarea").val("");
     });
+
+    $("#check").click(function () {
+        var project_name = $("#project").val();
+        var code = $("#code").val();
+
+        if (project_name == ""){
+            alert("案件名をご入力してください")
+        }else if (code == ""){
+            alert("番号をご入力してください")
+        }else {
+            $.ajax({
+                data: {project: project_name, code: code},
+                type: "get",
+                success: function (result) {
+                    $("#msg1").show();
+                    $("#msg2").hide();
+
+                    if (result == false){
+                        $("#result").text("このキーもう存在しています");
+                        $("#result").css('color', 'red')
+                    }else {
+                        $("#result").text("このキーは使用られることができます");
+                        $("#result").css('color', 'green')
+                    }
+                }
+            })
+        }
+    })
 });
--- routes/view.js
+++ routes/view.js
@@ -18,126 +18,147 @@ router.get('/', function (req, res) {
             throw err
         }else {
             var search = req.query;
-
-            for (var k in search){
-                if (search[k] == ""){
-                    delete search[k]
-                }
-            }
-
             var col_demands = db.collection('demands');
-            var summation = 0;
 
-            col_demands.find({}).toArray(function (err, item) {
-                if (err){
-                    throw err
-                }else {
-                    item.forEach(function () {
-                        summation += 1
-                    })
-                }
-            });
-            
-            if (search.search){
-                delete search.search;
-                var search_info = search;
-                var str = "";
-
-                if (search['project'] && search['code']){
-                    search['key'] = search['project'] +'-'+ search['code'];
-                    str = search.code;
-                    delete search.code;
-
-                    console.log(search);
-
-                    col_demands.find(search).toArray(function (err, doc) {
-                        if (err){
-                            throw err
-                        }else {
-                            search_info['code'] = str;
-                            res.render('view', {list: doc, search_info: search_info, summation: summation})
-                        }
-                    })
-                }else if (!search['project'] && search['code']){
-
-                    console.log(search);
-
-                    str = search.code;
-
-                    delete search.code;
-
-
-                    col_demands.find({$and: [{key: {$regex: str}}, search]}).toArray(function (err, doc) {
-                        if (err){
-                            throw err
-                        }else {
-                            search_info['code'] = str;
-
-                            res.render('view', {list: doc, search_info: search_info, summation: summation})
-                        }
-                    })
-                }else {
-
-                    console.log(search);
-
-                    col_demands.find(search).toArray(function (err, doc) {
-                        if (err){
-                            throw err
-                        }else {
-                            res.render('view', {list: doc, search_info: search_info, summation: summation})
-                        }
-                    })
-                }
-
-            }else if (search.condition && search.value){
-
-                var condition = search.condition;
-                var value = search.value;
+            if (search['project'] && search['code']){
                 
-                var search_data = {};
-                search_data[condition] = value;
+                var result = "";
 
+                var key = search['project'] +"-"+ search['code'];
 
-                col_demands.find(search_data).toArray(function (err, doc) {
+                col_demands.find({'key': key}).toArray(function (err, doc) {
                     if (err){
                         throw err
                     }else {
-                        if (doc.length == 0){
-                            res.render('view', {summation: summation})
-                        }else {
-                            res.render('view', {list: doc, summation: summation})
-                        }
-                    }
-                })
-            }else if (search.view_key){
-                col_demands.findOne({key: search.view_key}, function (err, doc) {
-                    if (err){
-                        throw err
-                    }else{
                         console.log(doc);
-                        res.render('modify', {info: doc})
-                    }
-                })
-            }else if (search.del_key){
-                col_demands.remove({'key': search.del_key}, function (err, doc) {
-                    if (err){
-                        throw err
-                    }else {
-                        res.redirect('/view')
+                        if (doc.length == 0){
+                            result = true;
+                        }else {
+                            result = false;
+                        }
+                        res.send(result)
                     }
                 })
             }else {
-                col_demands.find({}).toArray(function (err, doc) {
+                for (var k in search){
+                    if (search[k] == ""){
+                        delete search[k]
+                    }
+                }
+                
+                var summation = 0;
+
+                col_demands.find({}).toArray(function (err, item) {
                     if (err){
                         throw err
                     }else {
-                        if (doc.length == 0){
-                            res.render('view')
-                        }else {
-                            res.render('view', {list: doc, summation: summation})
-                        }
+                        item.forEach(function () {
+                            summation += 1
+                        })
                     }
-                })
+                });
+
+                if (search.search){
+                    delete search.search;
+                    var search_info = search;
+                    var str = "";
+
+                    if (search['project'] && search['code']){
+                        search['key'] = search['project'] +'-'+ search['code'];
+                        str = search.code;
+                        delete search.code;
+
+                        console.log(search);
+
+                        col_demands.find(search).toArray(function (err, doc) {
+                            if (err){
+                                throw err
+                            }else {
+                                search_info['code'] = str;
+                                res.render('view', {list: doc, search_info: search_info, summation: summation})
+                            }
+                        })
+                    }else if (!search['project'] && search['code']){
+
+                        console.log(search);
+
+                        str = search.code;
+
+                        delete search.code;
+
+
+                        col_demands.find({$and: [{key: {$regex: str}}, search]}).toArray(function (err, doc) {
+                            if (err){
+                                throw err
+                            }else {
+                                search_info['code'] = str;
+
+                                res.render('view', {list: doc, search_info: search_info, summation: summation})
+                            }
+                        })
+                    }else {
+
+                        console.log(search);
+
+                        col_demands.find(search).toArray(function (err, doc) {
+                            if (err){
+                                throw err
+                            }else {
+                                res.render('view', {list: doc, search_info: search_info, summation: summation})
+                            }
+                        })
+                    }
+
+                }else if (search.condition && search.value){
+
+                    var condition = search.condition;
+                    var value = search.value;
+
+                    var search_data = {};
+                    search_data[condition] = value;
+
+
+                    col_demands.find(search_data).toArray(function (err, doc) {
+                        if (err){
+                            throw err
+                        }else {
+                            if (doc.length == 0){
+                                res.render('view', {summation: summation})
+                            }else {
+                                res.render('view', {list: doc, summation: summation})
+                            }
+                        }
+                    })
+                }else if (search.view_key){
+                    col_demands.findOne({key: search.view_key}, function (err, doc) {
+                        if (err){
+                            throw err
+                        }else{
+                            console.log(doc);
+                            res.render('modify', {info: doc})
+                        }
+                    })
+                }else if (search.del_key){
+                    col_demands.remove({'key': search.del_key}, function (err, doc) {
+                        if (err){
+                            throw err
+                        }else {
+                            res.redirect('/view')
+                        }
+                    })
+                }else {
+                    col_demands.find({}).toArray(function (err, doc) {
+                        if (err){
+                            throw err
+                        }else {
+                            if (doc.length == 0){
+                                res.render('view')
+                            }else {
+                                res.render('view', {list: doc, summation: summation})
+                            }
+                        }
+                    })
+                }
             }
         }
     })
@@ -156,11 +177,15 @@ router.post('/', function (req, res) {
             modify['_id'] = ObjectId(modify['_id']);
             modify['key'] = modify['project'] +'-'+ modify['key'];
             var col_demands = db.collection('demands');
+            console.log(modify['_id']);
 
             col_demands.findOne({_id: modify['_id']}, function (err, doc) {
                 if (err){
                     throw err
                 }else {
+                    console.log('doc:'+doc);
+                    console.log('modify:'+modify);
+
                     if (doc.key != modify.key){
                         col_demands.find({key: modify.key}).toArray(function (err, docs) {
                             if (err){
